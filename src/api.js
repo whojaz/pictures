672acf7c878f487bd1ba3b58d08955b6320fe943
Jazmin G. Espinoza Luna <166566599+whojaz@users.noreply.github.com>
2024-04-17 14:27:34 -0700

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const searchImage = async(term) => {
+    const url = 
+    const response = await axios.get(url, {
+        params: {
+            query: 'cars'
+        }
+    })
+
+    console.log(response)
+}
+
+export default searchImage
