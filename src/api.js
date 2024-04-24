ded9f79ca4f6ba9c0ab0bdc7cc6ebd1f873baada
Jazmin G. Espinoza Luna <166566599+whojaz@users.noreply.github.com>
2024-04-24 00:25:29 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,9 +8,10 @@ const searchImage = async(term) => {
         }
     })
 
+    console.log(term)
+    console.log(response.data.results)
+    
     return response.data.results
-
-    console.log(response)
 }
 
 export default searchImage
